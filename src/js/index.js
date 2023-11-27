510645970efecda6c1bb60c085b0f8c9c0d06ae9
vx-clutch <owestness@gmail.com>
2023-11-26 20:24:26 -0500

    Updates to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -42,3 +42,14 @@ function displayLocalTimeInMichigan() {
 }
 
 displayLocalTimeInMichigan();
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
